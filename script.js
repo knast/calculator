56abb43647eb9490c86a571a014f2dbdf13492f1
knast <knast808@gmail.com>
2022-12-22 23:56:16 +0100

    Add event listeners to each digit and clear button

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ const subtractBtn = document.querySelector('.subtract');
 const multiplyBtn = document.querySelector('.multiply');
 const divideBtn = document.querySelector('.divide');
 
+const display = document.querySelector('.display-number');
+
+
+
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
@@ -35,3 +39,60 @@ function multiply(firstNumber, secondNumber) {
 function divide(firstNumber, secondNumber) {
     return firstNumber/secondNumber;
 }
+
+function operate(operator,firstNumber, secondNumber) {
+    let sum;
+    let subtraction;
+    let multiplication;
+    let divide;
+    if(operator == '+') {
+        sum = add(firstNumber+secondNumber);
+        return sum;
+    } else if(operator == '-') {
+        subtraction = subtract(firstNumber,secondNumber);
+        return subtraction;
+    } else if(operator == '*') {
+        multiplication = multiply(firstNumber,secondNumber);
+        return multiplication;
+    } else if(operator == '/') {
+        divide = divide(firstNumber, secondNumber);
+        return divide;
+    }
+}
+
+let evaluation = [];
+
+clearBtn.addEventListener('click', () => {
+    evaluation = [];
+    display.textContent = evaluation;
+})
+
+numberOne.addEventListener('click', () => {
+    evaluation.push(1);
+    const evaluationWithoutCommas = evaluation.join('');
+    display.textContent = evaluationWithoutCommas;
+})
+
+numberTwo.addEventListener('click', () => {
+    evaluation.push(2);
+    const evaluationWithoutCommas = evaluation.join('');
+    display.textContent = evaluationWithoutCommas;
+})
+
+numberThree.addEventListener('click', () => {
+    evaluation.push(3);
+    const evaluationWithoutCommas = evaluation.join('');
+    display.textContent = evaluationWithoutCommas;
+})
+
+numberFour.addEventListener('click', () => {
+    evaluation.push(4);
+    const evaluationWithoutCommas = evaluation.join('');
+    display.textContent = evaluationWithoutCommas;
+})
+
+numberFive.addEventListener('click', () => {
+    evaluation.push(5);
+    const evaluationWithoutCommas = evaluation.join('');
+    display.textContent = evaluationWithoutCommas;
+})
