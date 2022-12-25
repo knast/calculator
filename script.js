baba74a8f2c4de4876425132d2558480f56c13b9
knast <knast808@gmail.com>
2022-12-25 22:11:02 +0100

    Add the equals and clear functionality

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-const numberOne = document.querySelector('.one');
-const numberTwo = document.querySelector('.two');
-const numberThree = document.querySelector('.three');
-const numberFour = document.querySelector('.four');
-const numberFive = document.querySelector('.five');
-const numberSix = document.querySelector('.six');
-const numberSeven = document.querySelector('.seven');
-const numberEight = document.querySelector('.eight');
-const numberNine = document.querySelector('.nine');
-
-const clearBtn = document.querySelector('.clear');
-const equalsBtn = document.querySelector('.equals');
-
-const plusBtn = document.querySelector('.plus');
-const subtractBtn = document.querySelector('.subtract');
-const multiplyBtn = document.querySelector('.multiply');
-const divideBtn = document.querySelector('.divide');
-
-const display = document.querySelector('.display-number');
-
-
-
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
 
 function subtract(firstNumber, secondNumber) {
-    if(firstNumber > secondNumber) {
-        return firstNumber - secondNumber;
-    } else if(firstNumber < secondNumber) {
-        return secondNumber - firstNumber;
-    }
+    return firstNumber - secondNumber;
 }
 
 function multiply(firstNumber, secondNumber) {
@@ -37,16 +11,19 @@ function multiply(firstNumber, secondNumber) {
 }
 
 function divide(firstNumber, secondNumber) {
-    return firstNumber/secondNumber;
+    if(firstNumber == 0) {
+        return alert('ERROR');
+    } else {    
+        return firstNumber/secondNumber;
+    }
 }
-
 function operate(operator,firstNumber, secondNumber) {
     let sum;
     let subtraction;
     let multiplication;
     let divide;
     if(operator == '+') {
-        sum = add(firstNumber+secondNumber);
+        sum = add(firstNumber,secondNumber);
         return sum;
     } else if(operator == '-') {
         subtraction = subtract(firstNumber,secondNumber);
@@ -60,64 +37,142 @@ function operate(operator,firstNumber, secondNumber) {
     }
 }
 
-let firstNumber = [];
+const display = document.querySelector('.display-number');
 
-clearBtn.addEventListener('click', () => {
-    firstNumber = [];
-    display.textContent = firstNumber;
+
+const oneBtn = document.querySelector('.one');
+const twoBtn = document.querySelector('.two');
+const threeBtn = document.querySelector('.three');
+const fourBtn = document.querySelector('.four');
+const fiveBtn = document.querySelector('.five');
+const sixBtn = document.querySelector('.six');
+const sevenBtn = document.querySelector('.seven');
+const eightBtn = document.querySelector('.eight');
+const nineBtn = document.querySelector('.nine');
+
+const clearBtn = document.querySelector('.clear');
+const equalsBtn = document.querySelector('.equals');
+
+const plusBtn = document.querySelector('.plus');
+const subtractBtn = document.querySelector('.subtract');
+const multiplyBtn = document.querySelector('.multiply');
+const divideBtn = document.querySelector('.divide');
+
+const calculator = {
+    firstNum: 0,
+    secondNum: 0,
+    operatorSign: 'none'
+}
+
+let forNumber = [];
+let forNumToStr;
+
+
+
+
+oneBtn.addEventListener('click', () => {
+    forNumber.push(1);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberOne.addEventListener('click', () => {
-    firstNumber.push(1);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+twoBtn.addEventListener('click', () => {
+    forNumber.push(2);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberTwo.addEventListener('click', () => {
-    firstNumber.push(2);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+threeBtn.addEventListener('click', () => {
+    forNumber.push(3);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberThree.addEventListener('click', () => {
-    firstNumber.push(3);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+fourBtn.addEventListener('click', () => {
+    forNumber.push(4);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberFour.addEventListener('click', () => {
-    firstNumber.push(4);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+fiveBtn.addEventListener('click', () => {
+    forNumber.push(5);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberFive.addEventListener('click', () => {
-    firstNumber.push(5);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+sixBtn.addEventListener('click', () => {
+    forNumber.push(6);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberSix.addEventListener('click', () => {
-    firstNumber.push(6);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+sevenBtn.addEventListener('click', () => {
+    forNumber.push(7);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberSeven.addEventListener('click', () => {
-    firstNumber.push(7);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+eightBtn.addEventListener('click', () => {
+    forNumber.push(8);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberEight.addEventListener('click', () => {
-    firstNumber.push(8);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+nineBtn.addEventListener('click', () => {
+    forNumber.push(9);
+    forNumToStr = forNumber.join('');
+    display.textContent = forNumToStr;
 })
 
-numberNine.addEventListener('click', () => {
-    firstNumber.push(9);
-    const firstNumberWithoutCommas = firstNumber.join('');
-    display.textContent = firstNumberWithoutCommas;
+
+
+plusBtn.addEventListener('click', () => {
+    calculator.firstNum = Number(forNumToStr);
+    forNumber = [];
+    calculator.operatorSign = '+';
+    display.textContent = [];
+    
 })
+
+subtractBtn.addEventListener('click', () => {
+    calculator.firstNum = Number(forNumToStr);
+    forNumber = [];
+    calculator.operatorSign = '-';
+    display.textContent = [];
+})
+
+multiplyBtn.addEventListener('click', () => {
+    calculator.firstNum = Number(forNumToStr);
+    forNumber = [];
+    calculator.operatorSign = '*';
+    display.textContent = [];
+})
+
+divideBtn.addEventListener('click', () => {
+    calculator.firstNum = Number(forNumToStr);
+    forNumber = [];
+    calculator.operatorSign = '/';
+    display.textContent = [];
+})
+
+
+
+equalsBtn.addEventListener('click', () => {
+    calculator.secondNum = Number(forNumToStr);
+
+    let evaluation = operate(calculator.operatorSign, calculator.firstNum, calculator.secondNum);
+    
+    display.textContent = evaluation;
+    
+    
+})
+
+
+
+
+
+
+
+
+
 
